a7943abc5fb142be1d92454b7a08ccd0ff9d35b3
zephyr <brave_zephyr@163.com>
2019-11-20 07:57:10 +0000

    add InitLogger

--- common.h
+++ common.h
@@ -15,3 +15,15 @@ enum HttpMethod {
 int httpVersion=10
 volatile int timerExpired=0;
 
+// InitLogger init logger with logPath
+bool InitLogger(const std::string &logPath) {
+    try {
+        auto fileLogger = spdlog::basic_logger_mt("basic_logger", logPath.c_str());
+        spdlog::set_default_logger(fileLogger);
+    } catch (const spdlog::spdlog_ex &ex) {
+        std::cout << "Log Init Filed" << std::endl;
+        return false;
+    }
+    return true;
+}
+
