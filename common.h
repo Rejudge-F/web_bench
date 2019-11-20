475b1ad6f84a99cf3f2aa5d6cf937dc4f5f64d11
zephyr <brave_zephyr@163.com>
2019-11-20 02:30:30 +0000

    add common variables

---
+++ common.h
@@ -0,0 +1,15 @@
+enum HttpVersion {
+    HTTP_10 = 10,
+    HTTP_11 
+};
+
+enum HttpMethod {
+    METHOD_GET = 1,
+    METHOD_POST,
+    METHOD_OPTION,
+    METHOD_HEAD,
+    METHOD_TRACE
+};
+
+// global variable 
+int httpVersion=10
