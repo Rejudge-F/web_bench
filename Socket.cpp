e528def6c766005055be4fdbaa4cc92c3da048ce
zephyr <brave_zephyr@163.com>
2019-11-20 02:18:32 +0000

    update log method use spdlog

--- Socket.cpp
+++ Socket.cpp
@@ -1,8 +1,15 @@
 #include "Socket.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
 #include <string.h>
 #include <iostream>
+#include <stdio.h>
 
 Socket::Socket(const char *host, int clientPort) {
+    // init logger and put log into logs/basic.log
+    auto fileLogger = spdlog::basic_logger_mt("basic_logge", "logs/basic.log");
+    spdlog::set_default_logger(fileLogger);
+
     unsigned long inaddr;
     sockaddr_in ad;
     hostent *hp;
@@ -15,7 +22,9 @@ Socket::Socket(const char *host, int clientPort) {
     } else {
         hp = gethostbyname(host);
         if(hp == nullptr) {
-            std::cerr << "gethostbyname failed" << std::endl;
+            char errInfo[256];
+            sprintf(errInfo, "%s", strerror(errno));
+            SPDLOG_ERROR(errInfo);
             exit(1);
         }
         memcpy(&ad.sin_addr, hp->h_addr, hp->h_length);
@@ -24,11 +33,15 @@ Socket::Socket(const char *host, int clientPort) {
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock < 0) {
-        std::cerr << __FILE__ << " " << __LINE__ << "get socket failed" << std::endl;
+        char errInfo[256];
+        sprintf(errInfo, "%s", strerror(errno));
+        SPDLOG_ERROR(errInfo);
         exit(1);
     } else {
         if(connect(sock, (struct sockaddr*)&ad, sizeof(ad)) < 0) {
-            std::cerr << __FILE__ << " " << __LINE__ << "connect failed" << std::endl;
+            char errInfo[265];
+            sprintf(errInfo, "%s", strerror(errno));
+            SPDLOG_ERROR(errInfo);
             exit(1);
         }
     }
