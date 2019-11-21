8d15cdc8c25f11c75b7244f60f90947e1edd3c4f
zephyr <brave_zephyr@163.com>
2019-11-21 07:21:06 +0000

    add manager header

---
+++ bench_client_manager.h
@@ -0,0 +1,28 @@
+#ifndef BENCH_CLIENT_MANAGER_H
+#define BENCH_CLIENT_MANAGER_H
+
+#include "bench_client.h"
+#include "message_queue.h"
+#include <vector>
+#include <string>
+
+class BenchClientManager {
+public:
+    BenchClientManager();
+    BenchClientManager(force, reload, method, clients, proxyHost, proxyPort);
+    ~BenchClientManager();
+    BenchMark();
+private:
+    int _force;
+    int _reload;
+    int _method;
+    int _clients;
+    int _proxyPort;
+    clock_t sTime;
+    clock_t eTime;
+    std::string _proxyHost;
+    MessageQueue<BenchInfo> _messageQueue;
+    std::vector<std::shared_ptr<BenchClient> > _clients;
+};
+
+#endif // BENCH_CLIENT_MANAGER_H
