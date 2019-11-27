ffe59b5b8ea7990b5fdca78627933982692f47b5
Zephyr <brave_zephyr@163.com>
2019-11-27 07:57:08 +0000

    complete arguments format input

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include "common.h"
 #include <getopt.h>
 #include <signal.h>
+#include <unistd.h>
+#include <string>
+
+#define PROGRAM_VERSION "version: 1.0" 
 
 // define initial variables
 int force = 0;
 int reload = 0;
 int method = METHOD_GET;
-int clientsNum = 0;
-std::string proxyHost;
-int proxyPort;
-std::string url;
-int benchTime=1;
+int clientsNum = 1;
+std::string proxyHost = "localhost";
+int proxyPort=80;
+std::string url="http://localhost:80/index.html";
+int benchTime=30;
 
 // long option set 
 static const struct option long_options[] {
@@ -29,8 +33,99 @@ static const struct option long_options[] {
 	{nullptr, 0, nullptr, 0}
 };
 
+// usage help 
+static void usage() {
+	std::cerr << "webbench [option]... URL\n";
+	std::cerr << "-f|--force                Don't wait for server reply.\n";
+	std::cerr << "-r|--reload               Send reload request - Pragma:no-cache.\n";
+	std::cerr << "-t|--time <sec>           Run benchmark for <sec> seconds. Default 30.\n";
+    std::cerr << "-u|--url <server:port>    Request URL for this benchmark.\n";
+	std::cerr << "-c|--clients <n>          Run <n> clients in this benchmark.\n";
+	std::cerr << "-?|-h|--help              This Infomation.\n";
+	std::cerr << "--get                     Use GET request method.\n";
+	std::cerr << "--head                    Use HEAD request method.\n";
+	std::cerr << "--trace                   Use TRACE request method.\n";
+	std::cerr << "--options                 Use OPTIONS request method.\n";
+	std::cerr << "-V|--version              Display program version.\n";
+}
+
 int main(int argc, char *argv[])
 {
-	
+	int opt = 0;
+	int options_index = 0;
+
+	if(argc == 1) {
+		usage();
+		return 1;
+	}
+
+    while((opt = getopt_long(argc, argv, "Vfrt:u:c:?h", long_options, &options_index)) != EOF) {
+        switch (opt) {
+            case 'f':
+                force = 1;
+                break;
+            case 'r':
+                reload = 1;
+                break;
+            case 't':
+                benchTime = atoi(optarg);
+                break;
+            case 'V':
+                std::cout << PROGRAM_VERSION << std::endl;
+                exit(0);
+                break;
+            case 'u':
+            {
+                url = static_cast<std::string>(optarg); 
+                unsigned int index = url.find("http://", 0);
+                if (index != 0 || index == url.npos) {
+                    std::cerr << "URL support http protocol only.\n";
+                    exit(1);        
+                }
+                
+                // fine delimiter ':',  <server>:<port>
+                index = url.find(":", 7);
+                if (index == url.npos) {
+                    std::cerr << "URL losed port info.\n";
+                    exit(1);
+                }
+                // get host 
+                proxyHost = url.substr(7, index-7);
+
+                // get delimiter '/', <server>:<port>/URI
+                int tIndex = index + 1;
+                index = url.find("/", tIndex);
+                if (index == url.npos) {
+                    std::cerr << "URI has losed.\n";
+                    exit(1);
+                }
+                proxyPort = atoi(url.substr(tIndex, index - tIndex).c_str());
+                
+                break;
+            }
+            case 'c':
+                clientsNum = atoi(optarg);
+                break;
+            case '?':
+                usage();
+                break;
+            case 'h':
+                usage();
+                break;
+            default:
+                break;        
+        }
+    }
+
+    printf("* Bench mark Info...\n"); 
+    printf("* force:           %s\n", force?"true":"false");
+    printf("* reload:          %s\n", reload?"true":"false");
+    printf("* method:          %s\n", method==METHOD_GET?"GET":(method==METHOD_HEAD?"HEAD":(method==METHOD_OPTIONS?"OPTIONS":"TRACE")));
+    printf("* bench time:      %d sec\n", benchTime);
+    printf("* clients:         %d \n", clientsNum);
+    printf("* host:            %s\n", proxyHost.c_str());
+    printf("* port:            %d\n", proxyPort);
+    printf("* url:             %s\n", url.c_str());
+
 	return 0;
 }
